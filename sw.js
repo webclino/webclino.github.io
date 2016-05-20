16319cf2fa3dc5aa2a591d663efaed521cf70449
arijitlaik <laikarijit@gmail.com>
2016-05-20 08:21:40 +0530

    fc

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 importScripts('/cache-polyfill.js');
 
 const PREFIX = 'WebClino';
-const VER = 'v0.1.3-preAlpha-t22';
+const VER = 'v0.1.3-preAlpha-t222';
 const OFFLINE_CACHE = `${PREFIX}-${VER}`;
 
 var CACHE_URLS = [
